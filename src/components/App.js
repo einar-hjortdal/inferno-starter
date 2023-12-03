324e240ad2f8e1077b4b61722c8f68bbe69f45d8
Coachonko <info@coachonko.com>
2023-12-03 12:46:21 +0100

    Add info

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Routes from './Routes'
 
 export default function App () {
+  // The app component could be a more complex component with state and lifecycles.
   return (
     <div>
       <Routes />
